4348349378d10ebe71769863a4358156410e7b14
uptotec <mahmoudashraf11179@gmail.com>
2024-03-30 07:34:05 +0200

    used switch

--- src/scanner/utils.ts
+++ src/scanner/utils.ts
@@ -1,26 +1,27 @@
 import { TOKEN_TYPES } from './types';
 
 export function checkIdentifierType(currentToken: string) {
-  if (currentToken === 'read') {
-    return { type: TOKEN_TYPES.READ, value: currentToken };
-  } else if (currentToken === 'write') {
-    return { type: TOKEN_TYPES.WRITE, value: currentToken };
-  } else if (currentToken === 'if') {
-    return { type: TOKEN_TYPES.IF, value: currentToken };
-  } else if (currentToken === 'else') {
-    return { type: TOKEN_TYPES.ELSE, value: currentToken };
-  } else if (currentToken === 'then') {
-    return { type: TOKEN_TYPES.THEN, value: currentToken };
-  } else if (currentToken === 'end') {
-    return { type: TOKEN_TYPES.END, value: currentToken };
-  } else if (currentToken === 'repeat') {
-    return { type: TOKEN_TYPES.REPEAT, value: currentToken };
-  } else if (currentToken === 'until') {
-    return { type: TOKEN_TYPES.UNTIL, value: currentToken };
-  } else {
-    return {
-      type: TOKEN_TYPES.Identifier,
-      value: currentToken,
-    };
+  switch (currentToken) {
+    case 'read':
+      return { type: TOKEN_TYPES.READ, value: currentToken };
+    case 'write':
+      return { type: TOKEN_TYPES.WRITE, value: currentToken };
+    case 'if':
+      return { type: TOKEN_TYPES.IF, value: currentToken };
+    case 'else':
+      return { type: TOKEN_TYPES.ELSE, value: currentToken };
+    case 'then':
+      return { type: TOKEN_TYPES.THEN, value: currentToken };
+    case 'end':
+      return { type: TOKEN_TYPES.END, value: currentToken };
+    case 'repeat':
+      return { type: TOKEN_TYPES.REPEAT, value: currentToken };
+    case 'until':
+      return { type: TOKEN_TYPES.UNTIL, value: currentToken };
+    default:
+      return {
+        type: TOKEN_TYPES.Identifier,
+        value: currentToken,
+      };
   }
 }
